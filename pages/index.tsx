4e81234697ef0dd11f01870701972d6be3fcc20b
Ivan Sacara <isacara@fusionworks.md>
2023-03-03 13:05:25 +0200

    meta fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         <title>Ledneon</title>
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='Ledneon' content='Неон, лампочки, вывески, подсветка' />
+        <meta name='description' content='Неон, лампочки, вывески, подсветка' />
         <meta property='og:title' content='Ledneon' />
         <meta property='og:type' content='website' />
         <meta property='og:url' content='https://ledneon.md' />
